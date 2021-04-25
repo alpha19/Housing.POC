8a54813f98baad47f3b1a5d64f19ef2caa0aacd5
kdedow <kdedow@gmail.com>
2021-04-25 15:07:36 -0700

    More work on tutorial. Still only using react

--- src/components/TodoItem.js
+++ src/components/TodoItem.js
@@ -1,9 +1,20 @@
-import React from "react"
+import React, { Component } from "react"
 
-class TodoItem extends React.Component {
+class TodoItem extends Component {
 	render () {
 		return (
-			<li>{this.props.todo.title}</li>
+			<li>
+				<input 
+					type="checkbox" 
+					checked={this.props.todo.completed}
+					onChange={() => this.props.completeTodo(this.props.todo.id)}
+				/>
+				<button
+					onClick={() => this.props.deleteTodo(this.props.todo.id)}>
+					Delete
+				</button>
+				{this.props.todo.title}
+			</li>
 		)
 	}
 }
--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -1,13 +1,20 @@
-import React from "react"
+import React, { Component } from "react"
 
 import TodoItem from "./TodoItem"
 
-class TodoList extends React.Component {
+class TodoList extends Component {
 	render () {
 		return (
 		<ul>
 			{this.props.todos.map(todo => (
-				<TodoItem key={todo.id} todo={todo}></TodoItem>))}
+				<TodoItem 
+					key={todo.id} 
+					todo={todo} 
+					completeTodo={this.props.completeTodo}
+					deleteTodo={this.props.deleteTodo}>
+				</TodoItem>)
+				)
+			}
 		</ul>
 		)
 	}
