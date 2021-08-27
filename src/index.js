b73c85e9143fa5a7492db1bc750e46b5cba28947
kdedow <kdedow@gmail.com>
2021-08-26 22:38:02 -0700

    Pull out fucntionality

--- src/index.js
+++ src/index.js
@@ -1,8 +1,6 @@
 import React from "react"
 import ReactDOM from "react-dom"
 
-import { BrowserRouter as Router} from "react-router-dom"
-
 import CityContainer from "./components/CityContainer"
 
 //stylesheet
@@ -10,9 +8,7 @@ import "./app.css"
 
 ReactDOM.render(
 	<React.StrictMode>
-		<Router>
-			<CityContainer />
-		</Router>
+		<h1>fasdfas</h1>
 	</React.StrictMode>, 
 	document.getElementById("root")
 )
