17cea1e6e18a09a62202c3ca293e2278bcaabff9
kdedow <kdedow@gmail.com>
2021-08-26 22:40:44 -0700

    Fix some stuff

--- src/index.js
+++ src/index.js
@@ -1,14 +1,12 @@
 import React from "react"
 import ReactDOM from "react-dom"
 
-import CityContainer from "./components/CityContainer"
-
 //stylesheet
 import "./app.css"
 
 ReactDOM.render(
 	<React.StrictMode>
-		<h1>fasdfas</h1>
+		<h1>I AM KARL</h1>
 	</React.StrictMode>, 
 	document.getElementById("root")
 )
